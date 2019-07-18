ad4e34f003c0e38b4960879ebb80634905dcee35
TyhGB <30275349+TyhGB@users.noreply.github.com>
2019-07-18 14:41:37 +0800

    2

---
+++ A_section/Classes/AViewController.h
@@ -0,0 +1,17 @@
+//
+//  AViewController.h
+//  MainProject
+//
+//  Created by TyhOS on 2019/7/18.
+//  Copyright © 2019 上海佰量网络科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ A_section/Classes/AViewController.m
@@ -0,0 +1,42 @@
+//
+//  AViewController.m
+//  MainProject
+//
+//  Created by TyhOS on 2019/7/18.
+//  Copyright © 2019 上海佰量网络科技. All rights reserved.
+//
+
+#import "AViewController.h"
+#import "BViewController.h"
+@interface AViewController ()
+
+@end
+
+@implementation AViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    // Do any additional setup after loading the view.
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
+    [btn setFrame:CGRectMake(100, 100, 100, 100)];
+    [btn setTitle:@"A推B" forState:UIControlStateNormal];
+    [btn addTarget:self action:@selector(pushVC) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+}
+- (void)pushVC
+{
+    BViewController *vc = [[BViewController alloc] init];
+    [self.navigationController pushViewController:vc animated:YES];
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
